0ac612f4cf9a0b95f1106ac7ce5b1a39605f04f2
韦阿丰 <790107894@qq.com>
2018-11-29 17:34:06 +0800

    'ini'

---
+++ src/main/java/com/wei/demo/multithread/MultithreadApplication.java
@@ -0,0 +1,12 @@
+package com.wei.demo.multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadApplication.class, args);
+	}
+}
