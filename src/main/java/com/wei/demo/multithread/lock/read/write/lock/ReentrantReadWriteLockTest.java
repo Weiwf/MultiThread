f1aa3c3d13394b3238b7237e8a50e5e6e75cc524
wwf33 <wei_wenfeng33@163.com>
2018-12-06 23:48:45 +0800

    '添加读写锁例子'

--- src/main/java/com/wei/demo/multithread/lock/read/write/lock/ReentrantReadWriteLockTest.java
+++ src/main/java/com/wei/demo/multithread/lock/read/write/lock/ReentrantReadWriteLockTest.java
@@ -1,8 +1,93 @@
 package com.wei.demo.multithread.lock.read.write.lock;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @author weiwenfeng
  * @date 2018/12/6
  */
 public class ReentrantReadWriteLockTest {
+    public static void main(String[] args) {
+        MyCount myCount = new MyCount(1000);
+        User user = new User(myCount);
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    user.getMoney();
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    user.setMoney((int) (1000 * Math.random()));
+                }
+            }).start();
+        }
+    }
+}
+
+class User {
+    private ReadWriteLock readWriteLock;
+
+    private MyCount myCount;
+
+    public User(MyCount myCount) {
+        this.myCount = myCount;
+        readWriteLock = new ReentrantReadWriteLock();
+    }
+
+    public void getMoney() {
+        readWriteLock.readLock().lock();
+
+        try {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName +" get money start ...");
+            int count = myCount.getCount();
+            Thread.sleep((long) (100 * Math.random()));
+            System.out.println(threadName + " get " + count + " money!");
+            System.out.println(threadName + " get money end ...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+
+    }
+
+    public void setMoney(int count) {
+        readWriteLock.writeLock().lock();
+
+        try {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " ------------ set Money start ----------");
+            myCount.setCount(count);
+            System.out.println(threadName + " set " + count + " money!");
+            Thread.sleep(1000);
+            System.out.println(threadName + " ------------ set money end ------------");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+
+    }
+}
+
+class MyCount {
+    private int count;
+
+    public MyCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
